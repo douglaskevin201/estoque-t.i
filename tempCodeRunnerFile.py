a10a697b81320a2f9e9e231c6a7d3c7c13f3d8bf
KevinDouglas <douglas.kevin201@gmail.com>
2026-07-23 20:59:01 -0300

    criando a classe Item

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -11,6 +11,7 @@ class Item:
 
     def aumentar_estoque(self, quantidade):
         if quantidade <= 0:
+            print("Quantidade invalida!")
             raise ValueError("Numeros negativos não são permitidos.")
         else:
             self.quantidade += quantidade
@@ -18,12 +19,18 @@ class Item:
 
     def diminuir_estoque(self, quantidade):
         if quantidade <= 0:
+            print(f"Quantidade insuficiente no estoque. Qnt: {self.quantidade}")
             raise ValueError("Numeros negativos não são permitidos.")
         elif quantidade > self.quantidade:
+            print(f"Quantidade insuficiente no estoque. Qnt {self.quantidade}")
             raise ValueError("Quantidade insuficiente em estoque.")
         else:
             self.quantidade -= quantidade
 
     
+        
 
+listar_itens = Item()
+for itens in listar_itens:
+    print(f"{self.id} - {self.nome} - {self.quantidade}")
 
